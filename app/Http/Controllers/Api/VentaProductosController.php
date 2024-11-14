96e7390f18fdc5e8fce0adaf52489ca341a2397e
flashCrisisEnTierrasInfinitas <desarrollo3@ingeal.com>
2024-11-14 10:06:26 -0500

    nuevos ajustes

--- app/Http/Controllers/Api/VentaProductosController.php
+++ app/Http/Controllers/Api/VentaProductosController.php
@@ -9,12 +9,12 @@ use Illuminate\Support\Facades\DB;
 
 class VentaProductosController extends Controller
 {
-
+//
     public function index($id)
     {
         try {
             $data = DB::table('products')
-                ->select('id', 'nombrePro', 'codigoPro', 'precio', 'cantidad', 'img')
+                ->select('products.id', 'nombrePro', 'codigoPro', 'precio', 'cantidad', 'img')
                 ->join('venta_productos', 'products.id', '=', 'venta_productos.producto_id')
                 ->where('venta_productos.venta_id', '=', $id)
                 ->get();
